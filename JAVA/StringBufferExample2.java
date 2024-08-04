25792acddc2d99ca5a3c34126b50fc0325679475
LOKESH <plokeshwaran@gmail.com>
2024-08-04 17:23:10 +0530

    some addition

---
+++ JAVA/StringBufferExample2.java
@@ -0,0 +1,7 @@
+class StringBufferExample2{
+    public static void main(String args[]){
+        StringBuffer sb = new StringBuffer("Holaa");
+        sb.insert(1," JAVA");
+        System.out.println(sb);
+    }
+}
