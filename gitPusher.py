eac4d07a1af9dcb02e698b99a8aab3d60b85a0b0
Lokesh-Spectre <plokeshwaran@gmail.com>
2024-08-10 20:02:12 +0530

    Test message another time

--- gitPusher.py
+++ gitPusher.py
@@ -1,26 +1,38 @@
+import json
+import base64
 import os
 from git import Repo
 from getpass import getpass
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
 
+def keyGen(passwd):
+    salt = b'\xc6\xd0B\xbc_\xcf\x14\xdc\xa4\xe0\x036\xd2\xcb\xa8)'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=390000,
+    )
+    return base64.urlsafe_b64encode(kdf.derive(passwd.encode()))
 
 # Define your personal access token, username, and repository name
-token = getpass()
-username = "Lokesh-Spectre"
-repo_name = ""
-
-# Local repository path
-repo_dir = "/path/to/your/local/repo"
-
-# Initialize the repository object
+# token = getpass()
+# key = getpass()
+key="MyGitPass"
+fernet = Fernet(keyGen(key))
+creds = fernet.decrypt(open(".creds").read().encode()).decode()
+creds = json.loads(creds)
+# print(creds)
+# exit()
 repo = Repo()
-
-# Define the remote URL with the token
-remote_url = "https://github.com/Lokesh-vitcc/java-lab.git"
-
+remote_url = repo.remotes.origin.url
 # Temporarily set the remote URL using custom environment
-with repo.git.custom_environment(GIT_ASKPASS="echo", GIT_USERNAME=username, GIT_PASSWORD=token):
-    # Perform the push using the temporary remote URL
-    repo.git.push(remote_url, 'HEAD:refs/heads/main', '--force')
-
+COMMIT_MESSAGE="Test message another time"
+with repo.git.custom_environment(GIT_ASKPASS="echo", GIT_USERNAME=creds['username'], GIT_PASSWORD=creds["token"]):
+    repo.git.add(update=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    repo.git.push(remote_url)# , 'HEAD:refs/heads/main', '--force')
 # Clean up (optional)
 del token
